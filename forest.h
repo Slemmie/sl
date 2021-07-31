817876ea98f3aa803c29198c42e46dd9441261f1
Slemmie <thorvejeneriksen@gmail.com>
2021-07-31 20:06:16 +0200

    forest.h

---
+++ forest.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <sl/graph.h>
+
+//-//
+
+template <typename A> class Forest : public Graph <A> {
+	
+public:
+	
+	using Graph <A>::edges;
+	using Graph <A>::g;
+	using Graph <A>::size;
+	
+	Forest(int _size) :
+	Graph <A> (_size)
+	{ }
+	
+	int add(int from, int to, A weight = 1) override {
+		assert(from >= 0 && from < size && to >= 0 && to < size);
+		int ind = edges.size();
+		assert(ind < size - 1);
+		g[from].push_back(ind);
+		g[to].push_back(ind);
+		edges.push_back({ from, to, weight });
+		return ind;
+	}
+	
+};
