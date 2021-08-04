ae8b571c563e89222f9d49fbd4ed2cb68857f417
Slemmie <thorvejeneriksen@gmail.com>
2021-08-04 18:35:11 +0200

    topo_sort.h tweak

--- src/topo_sort.h
+++ src/topo_sort.h
@@ -1,39 +1,58 @@
 #pragma once
 
-#include <sl/graph.h>
+#include <vector>
+#include <iostream>
+#include <cassert>
 
 //-//
 
-template <typename A = int> class Topo_sort : public Graph <A> {
+class Topo_sort {
 	
 public:
 	
-	using Graph <A>::edges;
-	using Graph <A>::g;
-	using Graph <A>::size;
-	
+	std::vector <std::vector <int>> g;
 	std::vector <int> res, order;
+	int size;
 	
 	Topo_sort(int _size) :
-	Graph <A> (_size)
+	g(_size),
+	size(_size)
 	{ }
+	
+	void add(int from, int to) {
+		assert(from >= 0 && from < size && to >= 0 && to < size);
+		g[from].push_back(to);
+	}
 	
 	void reset() {
 		res.clear();
 		order = std::vector <int> (size, -1);
+		m_vis = std::vector <bool> (size, false);
 	}
 	
 	void init() {
 		reset();
 		for (int i = 0; i < size; i++) {
-			m_dfs(i);
+			if (!m_vis[i]) {
+				m_dfs(i);
+			}
 		}
+	}
+	
+	int& operator [] (int ind) {
+		assert(ind >= 0 && ind < size);
+		return res[ind];
+	}
+	
+	int operator [] (int ind) const {
+		assert(ind >= 0 && ind < size);
+		return res[ind];
 	}
 	
 	friend std::ostream& operator << (std::ostream& stream, const Topo_sort& g) {
 		stream << "{";
 		for (int i = 0; i < g.size; i++) {
-			stream << g.res[i];
+			stream << g[i];
 			if (i + 1 != g.size) {
 				stream << ", ";
 			}
@@ -45,14 +64,16 @@ public:
 private:
 	
 	void m_dfs(int node) {
-		if (order[node] != -1) {
-			return;
-		}
+		m_vis[node] = true;
 		for (int x : g[node]) {
-			m_dfs(edges[x].to);
+			if (!m_vis[x]) {
+				m_dfs(x);
+			}
 		}
 		order[node] = res.size();
 		res.push_back(node);
 	}
+	
+	std::vector <bool> m_vis;
 	
 };
