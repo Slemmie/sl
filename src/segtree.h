234788a82dd32cfa9985dbb854a66d333358db9a
Slemmie <thorvejeneriksen@gmail.com>
2021-08-14 16:43:08 +0200

    segtree.h tweak

--- src/segtree.h
+++ src/segtree.h
@@ -5,110 +5,96 @@
 
 //-//
 
-template <typename A, typename APPLY_ARG> class Segtree {
+template <typename node> class Segtree {
 	
 public:
 	
 	int size, n_size;
-	std::vector <A> v;
-	std::function <A(const A&, const A&)> merge;
-	std::function <void(A&, const APPLY_ARG&)> apply;
+	std::vector <node> v;
 	
-	Segtree(int _size,
-	const std::function <A(const A&, const A&)>& _merge,
-	const std::function <void(A&, const APPLY_ARG&)>& _apply) :
-	n_size(_size),
-	merge(_merge),
-	apply(_apply)
+	Segtree(int _size) :
+	n_size(_size)
 	{
 		assert(_size > 0);
 		size = 1;
 		while (size < _size) {
 			size <<= 1;
 		}
-		v.resize(size << 1, A { });
+		v.resize(size << 1, node { });
 		make();
 	}
 	
-	template <typename B> Segtree(const std::vector <B>& u,
-	const std::function <A(const A&, const A&)>& _merge,
-	const std::function <void(A&, const APPLY_ARG&)>& _apply) :
-	n_size(u.size()),
-	merge(_merge),
-	apply(_apply)
+	template <typename A> Segtree(const std::vector <A>& _v) :
+	n_size(_v.size())
 	{
-		assert(!u.empty());
+		assert(!_v.empty());
 		size = 1;
-		while (size < (int)u.size()) {
+		while (size < (int)_v.size()) {
 			size <<= 1;
 		}
-		v.resize(size << 1, A { });
-		make(u);
+		v.resize(size << 1, node { });
+		make(_v);
 	}
 	
-	template <typename B> Segtree(int _size, const std::vector <B>& u,
-	const std::function <A(const A&, const A&)>& _merge,
-	const std::function <void(A&, const APPLY_ARG&)>& _apply) :
-	n_size(_size),
-	merge(_merge),
-	apply(_apply)
+	template <typename A> Segtree(int _size, const std::vector <A>& _v) :
+	n_size(_size)
 	{
 		assert(_size > 0);
 		size = 1;
 		while (size < _size) {
 			size <<= 1;
 		}
-		v.resize(size << 1, A { });
-		make(u);
+		v.resize(size << 1, node { });
+		make(_v);
 	}
 	
 	void make() {
 		m_make(0, 0, size);
 	}
 	
-	template <typename B> void make(const std::vector <B>& u) {
-		m_make(u, 0, 0, size);
+	template <typename A> void make(const std::vector <A>& _v) {
+		m_make(_v, 0, 0, size);
 	}
 	
-	void update(int ind, const APPLY_ARG& arg) {
+	template <typename A> void update(int ind, const A& arg) {
 		assert(ind >= 0 && ind < size);
 		m_update(ind, 0, 0, size, arg);
 	}
 	
-	A query(int l, int r) {
+	node query(int l, int r) {
 		assert(l >= 0 && l < r && r <= size);
 		return m_query(l, r, 0, 0, size);
 	}
 	
-	A query() {
+	node query() {
 		return query(0, n_size);
 	}
 	
-	int find_first(int l, int r, const std::function <bool(const A&)>& check) {
+	int find_first(int l, int r, const std::function <bool(const node&)>& check) {
 		assert(l >= 0 && l < r && r <= size);
 		return m_find_first(l, r, 0, 0, size, check);
 	}
 	
-	int find_first(const std::function <bool(const A&)>& check) {
+	int find_first(const std::function <bool(const node&)>& check) {
 		return find_first(0, size, check);
 	}
 	
-	int find_last(int l, int r, const std::function <bool(const A&)>& check) {
+	int find_last(int l, int r, const std::function <bool(const node&)>& check) {
 		assert(l >= 0 && l < r && r <= size);
 		return m_find_last(l, r, 0, 0, size, check);
 	}
 	
-	int find_last(const std::function <bool(const A&)>& check) {
+	int find_last(const std::function <bool(const node&)>& check) {
 		return find_last(0, size, check);
 	}
 	
 private:
 	
-	int m_mid(int l, int r) {
+	inline int m_mid(int l, int r) {
 		return (l + r) >> 1;
 	}
 	
-	bool m_outside(int l, int r, int tl, int tr) {
+	inline bool m_outside(int l, int r, int tl, int tr) {
 		return l >= tr || r <= tl;
 	}
 	
@@ -119,25 +105,25 @@ private:
 		int mid = m_mid(l, r);
 		m_make((now << 1) + 1, l, mid);
 		m_make((now << 1) + 2, mid, r);
-		v[now] = merge(v[(now << 1) + 1], v[(now << 1) + 2]);
+		v[now] = node::merge(v[(now << 1) + 1], v[(now << 1) + 2]);
 	}
 	
-	template <typename B> void m_make(const std::vector <B>& u, int now, int l, int r) {
+	template <typename A> void m_make(const std::vector <A>& _v, int now, int l, int r) {
 		if (!(r - l - 1)) {
-			if (l < (int)u.size()) {
-				apply(v[now], APPLY_ARG(u[l]));
+			if (l < (int)_v.size()) {
+				v[now].apply(_v[l]);
 			}
 			return;
 		}
 		int mid = m_mid(l, r);
-		m_make(u, (now << 1) + 1, l, mid);
-		m_make(u, (now << 1) + 2, mid, r);
-		v[now] = merge(v[(now << 1) + 1], v[(now << 1) + 2]);
+		m_make(_v, (now << 1) + 1, l, mid);
+		m_make(_v, (now << 1) + 2, mid, r);
+		v[now] = node::merge(v[(now << 1) + 1], v[(now << 1) + 2]);
 	}
 	
-	void m_update(int ind, int now, int l, int r, const APPLY_ARG& arg) {
+	template <typename A> void m_update(int ind, int now, int l, int r, const A& arg) {
 		if (!(r - l - 1)) {
-			apply(v[now], arg);
+			v[now].apply(arg);
 			return;
 		}
 		int mid = m_mid(l, r);
@@ -146,22 +132,22 @@ private:
 		} else {
 			m_update(ind, (now << 1) + 2, mid, r, arg);
 		}
-		v[now] = merge(v[(now << 1) + 1], v[(now << 1) + 2]);
+		v[now] = node::merge(v[(now << 1) + 1], v[(now << 1) + 2]);
 	}
 	
-	A m_query(int tl, int tr, int now, int l, int r) {
+	node m_query(int tl, int tr, int now, int l, int r) {
 		if (l >= tl && r <= tr) {
 			return v[now];
 		}
 		int mid = m_mid(l, r);
-		A ret { };
+		node ret { };
 		if (m_outside(l, mid, tl, tr)) {
 			ret = m_query(tl, tr, (now << 1) + 2, mid, r);
 		} else {
 			if (m_outside(mid, r, tl, tr)) {
 				ret = m_query(tl, tr, (now << 1) + 1, l, mid);
 			} else {
-				ret = merge(
+				ret = node::merge(
 				m_query(tl, tr, (now << 1) + 1, l, mid),
 				m_query(tl, tr, (now << 1) + 2, mid, r));
 			}
@@ -170,7 +156,7 @@ private:
 	}
 	
 	int m_find_first_exists(int now, int l, int r,
-	const std::function <bool(const A&)>& check) {
+	const std::function <bool(const node&)>& check) {
 		if (!(r - l - 1)) {
 			return l;
 		}
@@ -182,7 +168,7 @@ private:
 	}
 	
 	int m_find_first(int tl, int tr, int now, int l, int r,
-	const std::function <bool(const A&)>& check) {
+	const std::function <bool(const node&)>& check) {
 		if (l >= tl && r <= tr) {
 			if (!check(v[now])) {
 				return -1;
@@ -201,7 +187,7 @@ private:
 	}
 	
 	int m_find_last_exists(int now, int l, int r,
-	const std::function <bool(const A&)>& check) {
+	const std::function <bool(const node&)>& check) {
 		if (!(r - l - 1)) {
 			return l;
 		}
@@ -213,7 +199,7 @@ private:
 	}
 	
 	int m_find_last(int tl, int tr, int now, int l, int r,
-	const std::function <bool(const A&)>& check) {
+	const std::function <bool(const node&)>& check) {
 		if (l >= tl && r <= tr) {
 			if (!check(v[now])) {
 				return -1;
