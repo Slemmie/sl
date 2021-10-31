eaea56ebd3d840d3bcf5969515ed7b2297ca50ac
Slemmie <thorvejeneriksen@gmail.com>
2021-10-31 18:37:09 +0100

    add next_bit_perm.h looparound on largest parameter value

--- src/next_bit_perm.h
+++ src/next_bit_perm.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <limits>
+
 //-//
 
 template <typename A> inline A next_bit_perm(A x) {
@@ -7,5 +9,8 @@ template <typename A> inline A next_bit_perm(A x) {
 		return 0;
 	}
 	A tmp = x | (x - 1);
+	if (tmp == std::numeric_limits <A>::max()) {
+		return x >> (__builtin_ffs(x) - 1);
+	}
 	return (tmp + 1) | ((~tmp & -(~tmp)) - 1) >> (__builtin_ffs(x));
 }
